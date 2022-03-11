738f1ada4ad262930e924a96d945bf4f34794f94
kafpliz <91686116+kafpliz@users.noreply.github.com>
2022-03-11 21:56:36 +0300

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+const size_t FPS = 50;
+const float WINDOW_WIDTH = 600;
+const float WINDOW_HEIGHT = 900;
+const std::string WINDOW_TITLE = "ORIENT";
+const float PLAYER_SPEEDX = 5.f;
